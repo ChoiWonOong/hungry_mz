977d116c3b0ad00994d9dd177ad5b3fb482c2617
ChoiWonOong <dnd5301@gmail.com>
2025-03-05 14:30:52 +0900

    FEAT: Add Restaurant Get APIs

--- hungry_mz/src/main/java/com/hungrymz/main/HungryMzApplication.java
+++ hungry_mz/src/main/java/com/hungrymz/main/HungryMzApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HungryMzApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HungryMzApplication.class, args);
 	}
--- hungry_mz/src/main/java/data/mapper/RestaurantMapper.java
+++ hungry_mz/src/main/java/data/mapper/RestaurantMapper.java
@@ -3,7 +3,10 @@ package data.mapper;
 import data.dto.RestaurantDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface RestaurantMapper {
     RestaurantDto getRestaurantById(int id);
+    List<RestaurantDto> getRestaurantList();
 }
